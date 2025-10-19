12ea9a6d770b0e818a2b5b6bbc0d73991406325f
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2025-10-19 08:34:50 -0600

    inicializacion de la base de datos

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import sys
+from core.database import init_db
 
+def main():
+    try:
+        init_db()
+    except Exception as e:
+        print(f"Error al inicializar la base de datos: {e}")
+        sys.exit(1)
 
 if __name__ == '__main__':
-    print("Hola mundo")
+    main()
